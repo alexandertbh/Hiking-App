7a45394e7ebe62b8a82fb01f2a1e2afff35a5940
Willlasbury <asburyw229@gmail.com>
2023-07-27 11:32:03 -0600

    added form to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import HikingForm from "@/components/HikingForm";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div></div>
-      </div>
-
-      <div className={styles.grid}>
-        <h2>
-          Docs <span>-&gt;</span>
-        </h2>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </div>
+      <HikingForm />
     </main>
   );
 }
---
+++ src/components/HikingForm.js
@@ -0,0 +1,11 @@
+export default function HikingForm () {
+    return (
+        <form>
+            <label>
+                Name:
+                <input type='text' name='name'></input>
+            </label>
+            <input type="submit" name="Submit"></input>
+        </form>
+    )
+}
